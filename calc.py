ca1149eb4f65943a0da2742fcd580898b68316ed
Alexander Danyarkhodzhaev <alexdany657@gmail.com>
2020-02-22 10:17:46 +0300

    First version

--- calc.py
+++ calc.py
@@ -1,17 +1,19 @@
 import consts
 from multiprocessing import Pool
-import math
 import json
+import time
+import math
+import cmath
 
 def ft(f_w, w, x, t):
-    summ = (0+0j)
-    for i in range(int(-(consts.k_0+.5)*consts.M),int((-consts.k_0+.5)*consts.M+1)):
+    summ = 0
+    for i in range(int(-(consts.k_0+abs(consts.k_0))*consts.M),int((-consts.k_0+abs(consts.k_0))*consts.M+1)):
         k = i / consts.M
-        summ += f_w(k) / consts.M * math.e ** ((0+1j)*(w(k)*t-k*x))
+        summ += f_w(k) / consts.M * cmath.exp((0+1j)*(w(k)*t-k*x)).real
     return summ
 
 w = lambda k: consts.w_0 * abs(k) ** consts.w_p
-f_w = lambda k: consts.A/2/math.pi**0.5*math.e**(-consts.A**2*(k+consts.k_0)**2/4)
+f_w = lambda k: consts.A/2/math.pi**0.5*math.exp(-consts.A**2*(k+consts.k_0)**2/4)
 f = lambda x,t: ft(f_w, w, x, t)
 
 def plot(t):
@@ -19,18 +21,24 @@ def plot(t):
     _x = []
     for __x in consts.x:
         _x.append(__x)
-        _y.append(f(__x, t).real)
+        _y.append(f(__x, t))
     print(t)
     return (_x, _y)
 
 def precalc() -> int:
+    zero = time.time()
     with Pool(4) as p:
         precalc = p.map(plot, consts.t)
     #
     dataFile = open(consts.constructFilename(), 'w')
     json.dump(precalc, dataFile)
     dataFile.close()
+    print(time.time() - zero)
+    return 0
+
+def main() -> int:
+    precalc()
     return 0
 
 if __name__ == "__main__":
-    precalc()
+    main()
--- consts.py
+++ consts.py
@@ -3,11 +3,11 @@ import numpy as np
 def constructFilename():
     return "linWave_"+str(A)+'_'+str(B)+'_'+str(k_0)+'_'+str(w_0)+'_'+str(w_p)+'_'+str(xLim)+'_'+str(xStep)+'_'+str(frames)+'_'+str(M)
 
-A = 10 #IMPORTANT
+A = 100 #IMPORTANT
 B = 1 #IMPORTANT
 k_0 = 1 #IMPORTANT
 w_0 = .5 #IMPORTANT
-w_p = 1.5 #VERY_IMPORTANT !!!
+w_p = 0.5 #VERY_IMPORTANT !!!
 
 xLim = 50 #IMPORTANT
 xStep = 1e-1 #IMPORTANT
@@ -18,6 +18,9 @@ xRange, yRange = [-xLim, xLim], [-1.1, 1.1]
 
 x = np.arange(-xLim,xLim+xStep,xStep)
 
-frames = 500 #IMPORTANT
+frames = 100 #IMPORTANT
 interval = 50
 t = np.arange(-frames//2,frames-frames//2,1)
+
+if __name__ == "__main__":
+    print(constructFilename())
